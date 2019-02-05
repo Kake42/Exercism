37384ad60a1b6125ff182f8c2fba5f04374c243d
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-02-05 12:39:32 -0700

    cshapr twofer

---
+++ Exercism/csharp/two-fer/TwoFer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+        string Name;
+        if (input == null) input = "you";
+        Name = ($"One for {input}, one for me.");
+        return Name;
+      }
+  }
