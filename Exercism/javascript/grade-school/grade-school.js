21243ff0b2ae6168d36104caee215ee6d778278b
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-02-11 18:59:24 -0700

    grade school

---
+++ Exercism/javascript/grade-school/grade-school.js
@@ -0,0 +1,27 @@
+export function GradeSchool () {
+
+    let roster = {};
+    let grades = [];
+
+    GradeSchool.prototype.roster = function(){
+       return JSON.parse(JSON.stringify(roster));
+    }
+
+    this.add = function(name, grade) {       
+        if (grades[grade] === undefined) { 
+            grades[grade] = []; 
+        }
+        grades[grade].push(name);
+        roster[grade] = grades[grade];
+        roster[grade].sort();
+        return this.roster();
+    };
+
+    //should return student list when passed in grade
+    this.grade = function(grade) {
+        if (roster[grade] === undefined) { 
+            return [];
+        }
+        return this.roster()[grade];
+    };
+} 
