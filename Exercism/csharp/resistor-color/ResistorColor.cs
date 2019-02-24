b4ff2cdc0a4465ec4b0d7afb940a2eb9fc883def
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-02-23 20:19:57 -0700

    colors

---
+++ Exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    static string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    public static int ColorCode(string color)
+    {
+        int code = Array.IndexOf(colors, color);
+        return code;
+    }
+
+    public static string[] Colors()
+    {
+        return colors;
+    }
+}
