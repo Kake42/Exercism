a45c25e97a313841f179944ee8ca1cdf66f7ed36
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-03-18 14:16:55 -0600

    grains!

---
+++ Exercism/csharp/grains/Grains.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if(n <= 0 || n > 64)
+        {
+            throw new System.ArgumentOutOfRangeException("no");
+        }
+        string binary = "1";
+        for(int i = 1; i < n; i++)
+        {
+            binary += "0";
+        }
+        var result = Convert.ToUInt64(binary, 2);
+        return result;
+    }
+
+    public static ulong Total()
+    {
+        ulong total = 0;
+         for(double i = 0; i < 64; i++)
+         {
+            // var temp = i + i;
+             total = total + (ulong)Math.Pow(2, i);
+         }
+        return total;
+    }
+}
