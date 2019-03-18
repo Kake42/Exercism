f211c28c0f82dac943816a5a528b70809fd22057
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-03-18 12:11:49 -0600

    reverse string

---
+++ Exercism/csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        //char[] word = input.ToCharArray();
+        //Array.Reverse(word);
+        //return new string(word);
+
+        //int l = input.Length - 1;
+        string result = "";
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            result += input[i];
+        }
+        return result;
+    }
+}
