059165047bc35484b1684b0a641771deefa8bce5
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-03-20 14:01:29 -0600

    proverb

---
+++ Exercism/csharp/proverb/Proverb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string[] result = new string[subjects.Length];
+        if(subjects.Length == 0)
+        {
+            return subjects;
+        }
+
+        for (int i = 0; i < subjects.Length - 1; i++)
+        {
+            string test = String.Format("For want of a {0} the {1} was lost.", 
+                        subjects[i], subjects[i+1]);
+            result[i] = test;
+        }
+        result[result.Length-1] = String.Format("And all for the want of a {0}.", subjects[0]);
+        return result;
+    }
+}
