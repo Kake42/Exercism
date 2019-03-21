6ddf2723a52048e31b4dcc0897c789e674215256
Kaitlyn Kakazu <KaitlynKakazu@gmail.com>
2019-03-21 12:19:51 -0600

    hello java

---
+++ Exercism/java/hello-world/src/main/java/Greeter.java
@@ -0,0 +1,9 @@
+class Greeter {
+
+    String getGreeting() {
+        String greet = "Hello, World!";
+        return greet;
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
